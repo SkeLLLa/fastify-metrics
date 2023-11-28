0057ce7268d67502a1766ad74af67fb24105de19
Neriuzz <neriusilmonas@gmail.com>
2023-11-28 16:07:29 +0000

    add test

--- src/__tests__/route-metrics.spec.ts
+++ src/__tests__/route-metrics.spec.ts
@@ -205,6 +205,66 @@ describe('route metrics', () => {
     });
   });
 
+  describe('{ enabled = {} }', () => {
+    let app = fastify();
+
+    afterEach(async () => {
+      await app.close();
+    });
+
+    beforeEach(async () => {
+      app = fastify();
+
+      await app.register(fastifyPlugin, {
+        endpoint: '/metrics',
+        routeMetrics: {
+          enabled: {},
+        },
+      });
+      app.get('/test', async () => {
+        return 'get test';
+      });
+      app.post('/test', async () => {
+        return 'post test';
+      });
+      await app.ready();
+    });
+
+    test('metrics exposed', async () => {
+      await expect(
+        app.inject({
+          method: 'GET',
+          url: '/test',
+        })
+      ).resolves.toBeDefined();
+
+      await expect(
+        app.inject({
+          method: 'POST',
+          url: '/test',
+        })
+      ).resolves.toBeDefined();
+
+      const metrics = await app.inject({
+        method: 'GET',
+        url: '/metrics',
+      });
+
+      expect(typeof metrics.payload).toBe('string');
+
+      const lines = metrics.payload.split('\n');
+
+      expect(lines).toEqual(
+        expect.arrayContaining([
+          'http_request_duration_seconds_count{method="GET",route="/test",status_code="200"} 1',
+          'http_request_duration_seconds_count{method="POST",route="/test",status_code="200"} 1',
+          'http_request_summary_seconds_count{method="GET",route="/test",status_code="200"} 1',
+          'http_request_summary_seconds_count{method="POST",route="/test",status_code="200"} 1',
+        ])
+      );
+    });
+  });
+
   describe('{ registeredRoutesOnly = false }', () => {
     let app = fastify();
 
